1eed76f0df8718b990c2bc4b2651ad3f3bf5e6d1
Danijel Wynyard <danijel.wynyard@judgejudy.local>
2023-11-27 01:07:09 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using LunaApiVultr;
+using LunaApiVultr.Models.Shared;
+
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiKey = "R5YR4UTYKS2BFSQ7J3NTGEFFMCPT3POIEDPA";
+
+        List<Region> regions = await Vultr.GetRegionsAsync(apiKey);
+
+        regions = regions.OrderBy(region => region.Country).ToList();
+
+        foreach (var i in regions)
+        {
+            Console.WriteLine(i.Id);
+            Console.WriteLine(i.City);
+            Console.WriteLine(i.Country);
+        }
+
+        Console.WriteLine("------------");
+
+        List<string> countries = await Vultr.GetCountriesAsync(apiKey);
+        foreach (var t in countries)
+        {
+            Console.WriteLine(t);
+        }
+
+        LunaApiVultr.Models.PlansAvailable? plans = await Vultr.GetAvailablePlansByRegionAsync(apiKey, "mel", "vc2");
+        foreach (var i in plans.AvailablePlans!)
+        {
+            Console.WriteLine(i);
+        }
+
+        var oSes = await Vultr.GetAvilableOsAsync(apiKey);
+        foreach (var o in oSes)
+        {
+            Console.WriteLine(o.Id);
+            Console.WriteLine(o.Name);
+            Console.WriteLine(o.Family);
+        }
+    }
+}
+
